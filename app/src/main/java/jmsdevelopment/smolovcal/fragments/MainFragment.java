b0ea2acfa0fce5bd5ade143f001ddca26843d7f8
Jesper Sørensen <Sorensen_hnd90@hotmail.com>
2016-07-07 22:42:40 +0200

    Reveal animations

--- app/src/main/java/jmsdevelopment/smolovcal/fragments/MainFragment.java
+++ app/src/main/java/jmsdevelopment/smolovcal/fragments/MainFragment.java
@@ -1,14 +1,20 @@
 package jmsdevelopment.smolovcal.fragments;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.DialogInterface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 
 import com.google.firebase.database.FirebaseDatabase;
@@ -63,5 +69,43 @@ public class MainFragment extends Fragment implements View.OnClickListener {
     public void onClick(View v) {
         if(v.getId() == floatingActionButton.getId()) {
         }
+    }
+
+    private void revealShow(View rootView, boolean reveal, final AlertDialog dialog){
+        final View view = rootView.findViewById(R.id.reveal_view);
+        int w = view.getWidth();
+        int h = view.getHeight();
+        float maxRadius = (float) Math.sqrt(w * w / 4 + h * h / 4);
+
+        if(reveal){
+            Animator revealAnimator = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                revealAnimator = ViewAnimationUtils.createCircularReveal(view,
+                        w / 2, h / 2, 0, maxRadius);
+            }
+
+            view.setVisibility(View.VISIBLE);
+            revealAnimator.start();
+
+        } else {
+
+            Animator anim =
+                    null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                anim = ViewAnimationUtils.createCircularReveal(view, w / 2, h / 2, maxRadius, 0);
+            }
+
+            anim.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    dialog.dismiss();
+
+                }
+            });
+
+            anim.start();
+        }
+
     }
 }
