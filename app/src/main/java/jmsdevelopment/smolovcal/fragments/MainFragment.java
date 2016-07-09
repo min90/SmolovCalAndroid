00d60da25041fa940cd3117d49b3fe6e44e12835
Jesper Sørensen <Sorensen_hnd90@hotmail.com>
2016-07-09 22:39:11 +0200

    New Junior Layout

--- app/src/main/java/jmsdevelopment/smolovcal/fragments/MainFragment.java
+++ app/src/main/java/jmsdevelopment/smolovcal/fragments/MainFragment.java
@@ -1,20 +1,14 @@
 package jmsdevelopment.smolovcal.fragments;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.content.DialogInterface;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 
 import com.google.firebase.database.FirebaseDatabase;
@@ -44,13 +38,15 @@ public class MainFragment extends Fragment implements View.OnClickListener {
         View view = inflater.inflate(R.layout.fragment_main_screen, container, false);
         floatingActionButton = (FloatingActionButton) view.findViewById(R.id.fabNewWorkout);
         floatingActionButton.setOnClickListener(this);
+        workouts.clear();
+        // Vi kalder onCreate view og der tilføjes til listen igen konstant efter tilbagevenden.
         workouts.add(new Workout("Bench Press", 10, true, false, 200));
         database = FirebaseDatabase.getInstance();
         setUpRecyclerView(view);
         return view;
     }
 
-    private void setUpRecyclerView(View view){
+    private void setUpRecyclerView(View view) {
         workoutsAdapter = new WorkoutsAdapter(workouts);
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewWorkouts);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -67,45 +63,10 @@ public class MainFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == floatingActionButton.getId()) {
+        if (v.getId() == floatingActionButton.getId()) {
+            FragmentController.get().transactFragments(getActivity(),
+                    NewWorkoutFragment.newInstance(floatingActionButton.getX(),
+                            floatingActionButton.getY()), "fragment_workout_junior");
         }
-    }
-
-    private void revealShow(View rootView, boolean reveal, final AlertDialog dialog){
-        final View view = rootView.findViewById(R.id.reveal_view);
-        int w = view.getWidth();
-        int h = view.getHeight();
-        float maxRadius = (float) Math.sqrt(w * w / 4 + h * h / 4);
-
-        if(reveal){
-            Animator revealAnimator = null;
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-                revealAnimator = ViewAnimationUtils.createCircularReveal(view,
-                        w / 2, h / 2, 0, maxRadius);
-            }
-
-            view.setVisibility(View.VISIBLE);
-            revealAnimator.start();
-
-        } else {
-
-            Animator anim =
-                    null;
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-                anim = ViewAnimationUtils.createCircularReveal(view, w / 2, h / 2, maxRadius, 0);
-            }
-
-            anim.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    super.onAnimationEnd(animation);
-                    dialog.dismiss();
-
-                }
-            });
-
-            anim.start();
-        }
-
     }
 }
--- app/src/main/java/jmsdevelopment/smolovcal/fragments/NewWorkoutFragment.java
+++ app/src/main/java/jmsdevelopment/smolovcal/fragments/NewWorkoutFragment.java
@@ -1,22 +1,60 @@
 package jmsdevelopment.smolovcal.fragments;
 
+import android.animation.Animator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+
+import jmsdevelopment.smolovcal.R;
 
 /**
  * Created by Jesper on 06/07/2016.
  */
 
 public class NewWorkoutFragment extends Fragment implements View.OnClickListener {
+    private static final String X_TAG = "x";
+    private static final String Y_TAG = "y";
+
+    public static NewWorkoutFragment newInstance(float x, float y) {
+
+        Bundle args = new Bundle();
+        args.putFloat(X_TAG, x);
+        args.putFloat(Y_TAG, y);
+        NewWorkoutFragment fragment = new NewWorkoutFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_workout_junior, container, false);
+
+        view.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                v.removeOnLayoutChangeListener(this);
+                int radius = (int) Math.hypot(right, bottom);
+
+                Bundle args = getArguments();
+                float cx = args.getFloat(X_TAG);
+                float cy = args.getFloat(Y_TAG);
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    Animator reveal = ViewAnimationUtils.createCircularReveal(v, (int) cx, (int) cy, 0, radius);
+                    reveal.setInterpolator(new DecelerateInterpolator());
+                    reveal.setDuration(600);
+                    reveal.start();
+                }
+
+            }
+        });
+        return view;
     }
 
     @Override
