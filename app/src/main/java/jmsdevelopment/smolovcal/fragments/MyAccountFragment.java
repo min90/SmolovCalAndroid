bd241fff4c30f97d1b6de9582f636504ca6a395e
Jesper Sørensen <Sorensen_hnd90@hotmail.com>
2016-07-13 22:08:48 +0200

    Hent af workouts

---
+++ app/src/main/java/jmsdevelopment/smolovcal/fragments/MyAccountFragment.java
@@ -0,0 +1,10 @@
+package jmsdevelopment.smolovcal.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Jesper on 13/07/2016.
+ */
+
+public class MyAccountFragment extends Fragment {
+}
---
+++ app/src/main/java/jmsdevelopment/smolovcal/fragments/WorkoutJuniorCycleFragment.java
@@ -0,0 +1,12 @@
+package jmsdevelopment.smolovcal.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Jesper on 13/07/2016.
+ */
+
+public class WorkoutJuniorCycleFragment extends Fragment {
+
+
+}
